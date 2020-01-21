13b8f23a7a4259c5ad885032468565da0076f404
Den <denysdiachenko1997@gmail.com>
2020-01-21 16:22:17 +0200

    Initialize project

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Ден on 21.01.2020.
+//  Copyright © 2020 Ден. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
